3ca64793c4d39ddb4e37d936b09358b264904d6c
Eduardo Garcia Misiuk <eduardogmisiuk@gmail.com>
2020-10-15 10:01:15 -0300

    Add preProcess unit test

---
+++ test/unit/configManager/preProcess/EnvVars.test.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-template-curly-in-string */
+const { EnvVars } = require('../../../../lib/configManager/preProcessors');
+
+describe('preProcess', () => {
+  it('should correctly add the env var value - env var is defined', () => {
+    process.env.TEST_VARIABLE = 'testDir';
+
+    expect(EnvVars.preProcess(['scope1.param1=/root/${TEST_VARIABLE}'])).toEqual([
+      'scope1.param1=/root/testDir',
+    ]);
+  });
+
+  it('should correctly add the env var value - env var is not defined', () => {
+
+    expect(EnvVars.preProcess(['scope1.param1=/root/${TEST_UNDEFINED_VARIABLE:-defaultDir}']))
+      .toEqual(['scope1.param1=/root/defaultDir']);
+  });
+});

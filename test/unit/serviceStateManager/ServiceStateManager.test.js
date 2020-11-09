7e411704e147cc1d1d54c73f6a7ca37745a631f9
Eduardo Garcia Misiuk <eduardogmisiuk@gmail.com>
2020-11-09 11:14:33 -0300

    Update tests

--- test/unit/serviceStateManager/ServiceStateManager.test.js
+++ test/unit/serviceStateManager/ServiceStateManager.test.js
@@ -1,30 +1,5 @@
-const { ServiceStateManager: { Manager } } = require('../../../index');
+const { Manager } = require('../../../lib/serviceStateManager');
 
-const mockWorker = {
-  on: jest.fn(),
-  postMessage: jest.fn(),
-};
-
-const mockMessagePort1 = {
-  close: jest.fn(),
-  on: jest.fn(),
-};
-
-const mockMessagePort2 = {
-  close: jest.fn(),
-  on: jest.fn(),
-  postMessage: jest.fn(),
-};
-
-const mockMessageChannel = {
-  port1: mockMessagePort1,
-  port2: mockMessagePort2,
-};
-
-jest.mock('worker_threads', () => ({
-  Worker: jest.fn(() => mockWorker),
-  MessageChannel: jest.fn(() => mockMessageChannel),
-}));
 jest.mock('express');
 jest.mock('lightship', () => ({
   createLightship: jest.fn(() => ({
@@ -36,217 +11,270 @@ jest.mock('lightship', () => ({
     shutdown: jest.fn(),
   })),
 }));
+jest.mock('logging/Logger.js', () => ({
+  Logger: jest.fn(() => ({
+    error: jest.fn(),
+    info: jest.fn(),
+  })),
+}));
 
-describe('constructor', () => {
-  it('should instantiate correctly - without worker threads', () => {
-    const manager = new Manager(['server']);
-
-    expect(manager.lightship).toBeDefined();
-    expect(manager.createBeacon).toBeDefined();
-    expect(manager.isServerReady).toBeDefined();
-    expect(manager.registerShutdownHandler).toBeDefined();
-    expect(manager.shutdown).toBeDefined();
-
-    expect(manager.worker).toBeUndefined();
-    expect(manager.signalingChannel).toBeUndefined();
-  });
-
-  it('should instantiate correctly - with worker threads', () => {
-    const manager = new Manager(['server'], { module: { 'worker.enable': true } });
-    manager.registerShutdownHandler = jest.fn();
-
-    expect(manager.lightship).toBeDefined();
-    expect(manager.createBeacon).toBeDefined();
-    expect(manager.isServerReady).toBeDefined();
-    expect(manager.registerShutdownHandler).toBeDefined();
-    expect(manager.shutdown).toBeDefined();
-
-    expect(manager.worker).toBeDefined();
-  });
-});
-
-describe('initWorker', () => {
-  let manager;
-
+describe('Manager', () => {
   beforeEach(() => {
-    manager = new Manager(['server'], { module: { 'worker.enable': true } });
-    manager.registerShutdownHandler = jest.fn();
-    manager.shutdown = jest.fn();
+    jest.clearAllMocks();
   });
 
-  it('should correctly initialize the worker', () => {
-    expect(manager.worker.on).toHaveBeenCalledTimes(2);
-    expect(manager.worker.on.mock.calls[0][0]).toBe('error');
-    expect(manager.worker.on.mock.calls[1][0]).toBe('online');
+  describe('constructor', () => {
+    it('should instantiate correctly - without config', () => {
+      const manager = new Manager(['server']);
 
-    // Testing the 'online' event callback
-    manager.worker.on.mock.calls[1][1]();
+      expect(manager.lightship).toBeDefined();
+      expect(manager.createBeacon).toBeDefined();
+      expect(manager.isServerReady).toBeDefined();
+      expect(manager.registerShutdownHandler).toBeDefined();
+      expect(manager.shutdown).toBeDefined();
 
-    expect(manager.signalingChannel).toBeDefined();
-    expect(manager.worker.postMessage).toHaveBeenCalledTimes(1);
-    expect(manager.worker.postMessage).toHaveBeenCalledWith(
-      { signalingChannel: mockMessagePort1 },
-      [mockMessagePort1],
-    );
-    expect(mockMessagePort2.on).toHaveBeenCalledTimes(1);
-    expect(mockMessagePort2.on).toHaveBeenCalledWith('message', expect.any(Function));
-    expect(manager.registerShutdownHandler).toHaveBeenCalledWith(expect.any(Function));
+      expect(manager.config).toBeDefined();
+      expect(manager.healthCheckers).toBeDefined();
+      expect(manager.healthCheckers).toBeInstanceOf(Map);
+      expect(manager.logger).toBeDefined();
+      expect(manager.serviceStatus).toBeDefined();
+    });
 
-    // Testing the shutdown function
-    manager.registerShutdownHandler.mock.calls[0][0]();
-    expect(mockMessagePort1.close).toHaveBeenCalledTimes(1);
-    expect(mockMessagePort2.close).toHaveBeenCalledTimes(1);
+    it('should instantiate correctly - config with different values from the default', () => {
+      const config = { lightship: { detectKubernetes: true } };
+      const manager = new Manager(['server'], config);
+
+      expect(manager.lightship).toBeDefined();
+      expect(manager.createBeacon).toBeDefined();
+      expect(manager.isServerReady).toBeDefined();
+      expect(manager.registerShutdownHandler).toBeDefined();
+      expect(manager.shutdown).toBeDefined();
+
+      expect(manager.config).toBeDefined();
+      expect(manager.config).toStrictEqual(config);
+      expect(manager.healthCheckers).toBeDefined();
+      expect(manager.healthCheckers).toBeInstanceOf(Map);
+      expect(manager.logger).toBeDefined();
+      expect(manager.serviceStatus).toBeDefined();
+    });
   });
 
-  it('should not correctly initialize the worker - an error occurs', () => {
-    expect(manager.worker.on).toHaveBeenCalledTimes(2);
-    expect(manager.worker.on.mock.calls[0][0]).toBe('error');
-    expect(manager.worker.on.mock.calls[1][0]).toBe('online');
+  describe('updateState', () => {
+    let manager;
+    let state;
 
-    // Testing the 'online' event callback
-    manager.worker.on.mock.calls[0][1]('testError');
+    describe('one state', () => {
+      beforeEach(() => {
+        manager = new Manager(['server']);
+        state = { server: true };
+      });
 
-    expect(manager.shutdown).toHaveBeenCalledTimes(1);
+      it('should correctly update the state - update to true and signal as ready', () => {
+        expect(() => manager.updateState(state)).not.toThrow();
+
+        expect(manager.serviceStatus).toEqual(state);
+        expect(manager.lightship.signalReady).toHaveBeenCalledTimes(1);
+      });
+
+      it('should correctly update the state - update to false and signal as not ready', () => {
+        state.server = false;
+
+        expect(() => manager.updateState(state)).not.toThrow();
+
+        expect(manager.serviceStatus).toEqual(state);
+        expect(manager.lightship.signalNotReady).toHaveBeenCalledTimes(1);
+      });
+    });
+
+    describe('two states', () => {
+      beforeEach(() => {
+        manager = new Manager(['server', 'db']);
+        state = { db: false, server: true };
+        manager.serviceStatus = state;
+      });
+
+      it('should correctly update one state - update db to true and signal as ready', () => {
+        state.db = true;
+
+        expect(() => manager.updateState({ db: true })).not.toThrow();
+
+        expect(manager.serviceStatus).toEqual(state);
+        expect(manager.lightship.signalReady).toHaveBeenCalledTimes(1);
+      });
+
+      it('should correctly update one state - update db to false and signal as not ready', () => {
+        expect(() => manager.updateState({ db: false })).not.toThrow();
+
+        expect(manager.serviceStatus).toEqual(state);
+        expect(manager.lightship.signalNotReady).toHaveBeenCalledTimes(1);
+      });
+    });
+
+    describe('error', () => {
+      beforeEach(() => {
+        manager = new Manager(['server']);
+      });
+
+      it('service state is not a boolean', () => {
+        state = { server: 'testState' };
+        expect(() => manager.updateState(state)).toThrowError(
+          `Invalid state type: expected "boolean", received "${typeof state.server}"`,
+        );
+      });
+
+      it('service not registered', () => {
+        expect(() => manager.updateState({ db: true })).toThrowError('Service is not registered');
+      });
+    });
   });
-});
 
-describe('handleSignalMessages', () => {
-  let manager;
-  let state;
+  describe('signalReady', () => {
+    it('should send a ready signal', () => {
+      const manager = new Manager(['server']);
+      manager.updateState = jest.fn();
 
-  beforeEach(() => {
-    manager = new Manager(['server'], { module: { 'worker.enable': true } });
-    manager.signalingChannel = mockMessageChannel;
-    manager.updateState = jest.fn();
-    manager.shutdown = jest.fn();
+      manager.signalReady('server');
 
-    state = { server: true };
+      expect(manager.updateState).toHaveBeenCalledTimes(1);
+      expect(manager.updateState).toHaveBeenCalledWith({ server: true });
+    });
   });
 
-  it('should correctly handle the message', () => {
-    manager.handleSignalMessages(state);
+  describe('signalNotReady', () => {
+    it('should send a not ready signal', () => {
+      const manager = new Manager(['server']);
+      manager.updateState = jest.fn();
 
-    expect(manager.shutdown).not.toHaveBeenCalled();
-    expect(manager.updateState).toHaveBeenCalledTimes(1);
-    expect(manager.updateState).toHaveBeenCalledWith(state);
-    expect(mockMessagePort2.postMessage).not.toHaveBeenCalled();
+      manager.signalNotReady('server');
+
+      expect(manager.updateState).toHaveBeenCalledTimes(1);
+      expect(manager.updateState).toHaveBeenCalledWith({ server: false });
+    });
   });
 
-  it('should not correctly handle the message - more than one state in the message', (done) => {
-    state.db = false;
+  describe('addHealthChecker', () => {
+    const service = 'server';
+    const interval = 1000;
 
-    manager.shutdown = () => {
-      // It will not stop the test, but if it is not called, will fail
+    let manager;
+    let healthChecker;
+
+    beforeEach(() => {
+      manager = new Manager(['server']);
+      healthChecker = jest.fn();
+      jest.useFakeTimers();
+    });
+
+    afterEach(() => {
+      jest.useRealTimers();
+    });
+
+    it('should add a new health checker', (done) => {
+      expect.assertions(4);
+
+      manager.addHealthChecker(service, healthChecker, interval);
+
+      expect(manager.healthCheckers.size).toBe(1);
+      expect(manager.healthCheckers.get(service)).toBeDefined();
+
+      expect(setInterval).toHaveBeenCalledTimes(1);
+      expect(setInterval).toHaveBeenCalledWith(expect.any(Function), interval);
+
       done();
-    };
-
-    manager.handleSignalMessages(state);
-  });
-
-  it('should not correctly handle the message - the updateState has thrown an error', () => {
-    manager.updateState = jest.fn(() => {
-      throw new Error('testError');
     });
 
-    manager.handleSignalMessages(state);
+    describe('setInterval function', () => {
+      beforeEach(() => {
+        manager.addHealthChecker(service, healthChecker, interval);
+      });
 
-    expect(manager.shutdown).not.toHaveBeenCalled();
-    expect(manager.updateState).toHaveBeenCalledTimes(1);
-    expect(manager.updateState).toHaveBeenCalledWith(state);
-    expect(mockMessagePort2.postMessage).toHaveBeenCalledTimes(1);
-    expect(mockMessagePort2.postMessage).toHaveBeenCalledWith({ error: 'testError' });
+      it('should execute the health checker function', (done) => {
+        expect.assertions(2);
+
+        jest.runTimersToTime(interval);
+
+        expect(healthChecker).toHaveBeenCalledTimes(1);
+        expect(healthChecker).toHaveBeenCalledWith(expect.any(Function), expect.any(Function));
+
+        done();
+      });
+
+      it('should not execute the health checker function - it is already running', (done) => {
+        expect.assertions(2);
+        manager.healthCheckers.get = jest.fn(() => ({ inUse: true }));
+
+        jest.runTimersToTime(interval);
+
+        expect(healthChecker).not.toHaveBeenCalled();
+        expect(healthChecker).not.toHaveBeenCalledWith(expect.any(Function), expect.any(Function));
+
+        done();
+      });
+    });
   });
-});
 
-describe('updateState', () => {
-  let manager;
-  let state;
+  describe('clearHealthChecker', () => {
+    let manager;
 
-  describe('one state', () => {
     beforeEach(() => {
       manager = new Manager(['server']);
-      state = { server: true };
+      jest.useFakeTimers();
     });
 
-    it('should correctly update the state - update to true and signal as ready', () => {
-      expect(() => manager.updateState(state)).not.toThrow();
-
-      expect(manager.serviceStatus).toEqual(state);
-      expect(manager.lightship.signalReady).toHaveBeenCalledTimes(1);
+    afterEach(() => {
+      jest.useRealTimers();
     });
 
-    it('should correctly update the state - update to false and signal as not ready', () => {
-      state.server = false;
+    it('should clear a health checker', (done) => {
+      expect.assertions(4);
 
-      expect(() => manager.updateState(state)).not.toThrow();
+      const func = jest.fn();
+      const interval = 1000;
 
-      expect(manager.serviceStatus).toEqual(state);
-      expect(manager.lightship.signalNotReady).toHaveBeenCalledTimes(1);
-    });
-  });
+      manager.addHealthChecker('server', func, interval);
+      expect(manager.healthCheckers.size).toBe(1);
+      manager.clearHealthChecker('server');
+      expect(manager.healthCheckers.size).toBe(0);
+      expect(manager.healthCheckers.get('server')).toBeUndefined();
+      expect(clearInterval).toHaveBeenCalled();
 
-  describe('two states', () => {
-    beforeEach(() => {
-      manager = new Manager(['server', 'db']);
-      state = { db: false, server: true };
-      manager.serviceStatus = state;
-    });
-
-    it('should correctly update one state - update db to true and signal as ready', () => {
-      state.db = true;
-
-      expect(() => manager.updateState({ db: true })).not.toThrow();
-
-      expect(manager.serviceStatus).toEqual(state);
-      expect(manager.lightship.signalReady).toHaveBeenCalledTimes(1);
+      done();
     });
 
-    it('should correctly update one state - update db to false and signal as not ready', () => {
-      expect(() => manager.updateState({ db: false })).not.toThrow();
+    it('should not clear a health checker - not registered', (done) => {
+      expect.assertions(1);
 
-      expect(manager.serviceStatus).toEqual(state);
-      expect(manager.lightship.signalNotReady).toHaveBeenCalledTimes(1);
+      manager.clearHealthChecker('server');
+
+      expect(clearInterval).not.toHaveBeenCalled();
+
+      done();
     });
   });
 
-  describe('error', () => {
+  describe('clearAllHealthCheckers', () => {
+    let manager;
+
     beforeEach(() => {
       manager = new Manager(['server']);
     });
 
-    it('service state is not a boolean', () => {
-      state = { server: 'testState' };
-      expect(() => manager.updateState(state)).toThrowError(
-        `Invalid state type: expected "boolean", received "${typeof state.server}"`,
-      );
+    it('should remove all health checkers', () => {
+      manager.addHealthChecker('server', jest.fn(), 1000);
+      manager.clearHealthChecker = jest.fn();
+
+      manager.clearAllHealthCheckers();
+
+      expect(manager.clearHealthChecker).toHaveBeenCalledTimes(1);
+      expect(manager.healthCheckers.size).toEqual(0);
     });
 
-    it('service not registered', () => {
-      expect(() => manager.updateState({ db: true })).toThrowError('Service is not registered');
+    it('should not remove the health checkers - there is no health checker', () => {
+      manager.clearHealthChecker = jest.fn();
+
+      manager.clearAllHealthCheckers();
+
+      expect(manager.clearHealthChecker).not.toHaveBeenCalled();
     });
-  });
-});
-
-describe('signalReady', () => {
-  it('should send a ready signal', () => {
-    const manager = new Manager(['server']);
-    manager.updateState = jest.fn();
-
-    manager.signalReady('server');
-
-    expect(manager.updateState).toHaveBeenCalledTimes(1);
-    expect(manager.updateState).toHaveBeenCalledWith({ server: true });
-  });
-});
-
-describe('signalNotReady', () => {
-  it('should send a not ready signal', () => {
-    const manager = new Manager(['server']);
-    manager.updateState = jest.fn();
-
-    manager.signalNotReady('server');
-
-    expect(manager.updateState).toHaveBeenCalledTimes(1);
-    expect(manager.updateState).toHaveBeenCalledWith({ server: false });
   });
 });
